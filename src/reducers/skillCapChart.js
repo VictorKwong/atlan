96efacf1b52cda36feed57634a7fdb70def8c47e
victorkaiwong <victorkaihong@gmail.com>
2022-07-22 22:15:37 -0400

    add skillCapChart.BashDamage

--- src/reducers/skillCapChart.js
+++ src/reducers/skillCapChart.js
@@ -1,10 +1,13 @@
 const Fn = {
+    //Level
     Bash: 5,
     Mammonite: 10,
     Kodoku: 13,
     MagnumBreak: 20,
     Quicken: 35,
     BowlingBash: 70,
+    //Damage Amplifier
+    BashDamage: 2.5,
 }
 
 
